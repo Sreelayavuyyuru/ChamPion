3bd0f39c9dad0506a03ebbd6716e4b3d6036cfb7
Sreelaya <>
2021-05-17 12:59:36 +0000

    Updated replies

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 import discord
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("http://quotes.stormconsultancy.co.uk/random.json")
+  json_data = json.loads(response.text)
+  quote = json_data['quote'] + " -" + json_data['author']
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -12,6 +20,10 @@ async def on_message(message):
   if(message.author == client.user):
     return
   if(message.content.startswith('.round challenge') or message.content.startswith('.match challenge')):
-    await message.channel.send('Hey, What\'s up!')
-
+    await message.channel.send("Hey, I can see you are about to start a challenge. Keep Going!!")
+  if 'Round over!' in message.content:
+    quote = get_quote()
+    await message.channel.send("Now that the round is completed, Here is a quote for you!!")
+    await message.channel.send(quote)
+    
 client.run(os.getenv('TOKEN'))
