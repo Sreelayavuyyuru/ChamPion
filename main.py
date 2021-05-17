834124b820a8fcd6328ab86680e9b1092b7dbfe9
Sreelaya <>
2021-05-17 17:44:18 +0000

    Read embed messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 import requests
 import json
+# import embed
 from keep_alive import keep_alive
 
 client = discord.Client()
@@ -17,18 +18,26 @@ async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
+
 async def on_message(message):
   if(message.author == client.user):
     return
-  if(message.content.startswith('.round challenge') or message.content.startswith('.match challenge')):
+  if('.round challenge' in message.content or '.match challenge' in message.content):
     await message.channel.send("Hey, I can see you are about to start a challenge. Keep Going!!")
-  if 'Round over!' in message.content:
-    quote = get_quote()
-    await message.channel.send("Now that the round is completed, Here is a quote for you!!")
-    await message.channel.send(quote)
-    await message.channel.send('Work harder, come back for another challenge, we believe in you!')  
-  if 'match has been invalidated' in message.content:
-    await message.channel.send('Come on, Don\'t give up!\nPractice harder!')
+  for i in message.embeds:
+    if 'Round over!' in i.description:
+      quote = get_quote()
+      await message.channel.send("Now that the round is completed, Here is a quote for you!!")
+      await message.channel.send(quote)
+      await message.channel.send('Work harder, come back for another challenge, we believe in you!')  
+  for i in message.embeds:
+    # print(i.description + " WTFWTFWTFWTF\n ")
+    if('match has been invalidated' in i.description):
+  #if 'Try mentioning the user' in embed.description:
+
+  # if flag == 1 and message.content.startswith('yes'):
+      await message.channel.send('Come on, Don\'t give up!\nPractice harder!')
+  #   flag = 0
     
 keep_alive()
 client.run(os.getenv('TOKEN'))
