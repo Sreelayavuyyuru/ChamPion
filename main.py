4e6fb4ff092bb2f6b7af4473e44a42829486230f
Sreelaya <>
2021-05-22 11:20:43 +0000

    Updated the round over message

--- main.py
+++ main.py
@@ -24,12 +24,47 @@ async def on_message(message):
     return
   if('.round challenge' in message.content or '.match challenge' in message.content):
     await message.channel.send("Hey, I can see you are about to start a challenge. Keep Going!!")
-  for i in message.embeds:
-    if 'Round over!' in i.description:
-      quote = get_quote()
-      await message.channel.send("Now that the round is completed, Here is a quote for you!!")
-      await message.channel.send(quote)
-      await message.channel.send('Work harder, come back for another challenge, we believe in you!')  
+  
+  print(message.content)
+  if("there is an update in standings" in message.content):
+    quote = get_quote()
+    await message.channel.send("Yay, There is an update in the standings!")
+    await message.channel.send(quote)
+    await message.channel.send('You guys can do better!!')
+
+  # if(type(message.embeds) is list) : 
+  #   for i in message.embeds:
+  #     print(i.title)
+  #     print(i.fields)
+  #     print(i.author)
+  #     print(i.description)
+  #     print(i.footer.text)
+  #     #   print(i.author.name)
+
+  #     print(i.fields)
+  #     if i.fields:
+  #       for x in i.fields:
+          # if("there is an update in standings" in x.name or "there is an update in standings" in x.value):
+                
+
+
+        #   if(i.author.name == 'Problems'):
+        #     print("YES WHATEVER") 
+      # else:
+      #   print('List is empty duh')
+      # print("SGFHGJDZHSGDJTDNHF")
+      # print(i.title)
+      # print("WTFWGJ")
+      # print("\n");
+      # print(i.description)
+      # print("WFTYGKHUKGJFH")
+
+  # if 'Lockout' in message.embeds.description or 'Lockout' in message.embeds.title:
+  #   print(message.embed.title + "WTFTWTFTWFJGKJ" + message.embed.description)
+    # quote = get_quote()
+    # await message.channel.send("Now that the round is completed, Here is a quote for you!!")
+    # await message.channel.send(quote)
+    # await message.channel.send('Work harder, come back for another challenge, we believe in you!')  
   for i in message.embeds:
     # print(i.description + " WTFWTFWTFWTF\n ")
     if('match has been invalidated' in i.description):
