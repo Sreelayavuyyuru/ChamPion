b23ce6732c4f46459c2429eb715e7cf671aa8942
Sreelaya <63154588+Sreelayavuyyuru@users.noreply.github.com>
2021-06-08 13:33:24 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 import requests
 import json
-# import embed
+
 from keep_alive import keep_alive
 
 client = discord.Client()
@@ -30,49 +30,9 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send("Yay, There is an update in the standings!")
     await message.channel.send(quote)
-    await message.channel.send('You guys can do better!!')
-
-  # if(type(message.embeds) is list) : 
-  #   for i in message.embeds:
-  #     print(i.title)
-  #     print(i.fields)
-  #     print(i.author)
-  #     print(i.description)
-  #     print(i.footer.text)
-  #     #   print(i.author.name)
-
-  #     print(i.fields)
-  #     if i.fields:
-  #       for x in i.fields:
-          # if("there is an update in standings" in x.name or "there is an update in standings" in x.value):
-                
-
-
-        #   if(i.author.name == 'Problems'):
-        #     print("YES WHATEVER") 
-      # else:
-      #   print('List is empty duh')
-      # print("SGFHGJDZHSGDJTDNHF")
-      # print(i.title)
-      # print("WTFWGJ")
-      # print("\n");
-      # print(i.description)
-      # print("WFTYGKHUKGJFH")
-
-  # if 'Lockout' in message.embeds.description or 'Lockout' in message.embeds.title:
-  #   print(message.embed.title + "WTFTWTFTWFJGKJ" + message.embed.description)
-    # quote = get_quote()
-    # await message.channel.send("Now that the round is completed, Here is a quote for you!!")
-    # await message.channel.send(quote)
-    # await message.channel.send('Work harder, come back for another challenge, we believe in you!')  
+    await message.channel.send('You can do better!!')
   for i in message.embeds:
-    # print(i.description + " WTFWTFWTFWTF\n ")
     if('match has been invalidated' in i.description):
-  #if 'Try mentioning the user' in embed.description:
-
-  # if flag == 1 and message.content.startswith('yes'):
       await message.channel.send('Come on, Don\'t give up!\nPractice harder!')
-  #   flag = 0
-    
 keep_alive()
 client.run(os.getenv('TOKEN'))
